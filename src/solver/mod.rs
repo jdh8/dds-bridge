2369ad269dfa4a1b0a11de6b1760c17226b17ba6
Chen-Pang He <chen.pang.he@jdh8.org>
2025-02-09 14:52:42 +0800

    Rename `Vulnerability::rotate` from `swap` to avoid confusion with swapping two instances

--- src/solver/mod.rs
+++ src/solver/mod.rs
@@ -481,32 +481,32 @@ impl Vulnerability {
         }
     }
 
-    /// Conditionally swap bits
+    /// Conditionally swap [`NS`](Self::NS) and [`EW`](Self::EW)
     ///
     /// This method makes rotating the vulnerability pair easy.  This method is
     /// `const` and `#[inline]` to maximize chances of constant folding
-    /// `.swap(true)`.
+    /// `.rotate(true)`.
     #[must_use]
     #[inline]
-    pub const fn swap(self, condition: bool) -> Self {
+    pub const fn rotate(self, condition: bool) -> Self {
         Self::from_bits_truncate(self.bits() * (condition as u8 * 3 + 2) / 2)
     }
 }
 
-/// Exhaustively check correctness of [`Vulnerability::swap`] at compile time
+/// Exhaustively check correctness of [`Vulnerability::rotate`] at compile time
 const _: () = {
     const ALL: Vulnerability = Vulnerability::all();
     const NONE: Vulnerability = Vulnerability::empty();
 
-    assert!(matches!(ALL.swap(true), ALL));
-    assert!(matches!(NONE.swap(true), NONE));
-    assert!(matches!(Vulnerability::NS.swap(true), Vulnerability::EW));
-    assert!(matches!(Vulnerability::EW.swap(true), Vulnerability::NS));
+    assert!(matches!(ALL.rotate(true), ALL));
+    assert!(matches!(NONE.rotate(true), NONE));
+    assert!(matches!(Vulnerability::NS.rotate(true), Vulnerability::EW));
+    assert!(matches!(Vulnerability::EW.rotate(true), Vulnerability::NS));
 
-    assert!(matches!(ALL.swap(false), ALL));
-    assert!(matches!(NONE.swap(false), NONE));
-    assert!(matches!(Vulnerability::NS.swap(false), Vulnerability::NS));
-    assert!(matches!(Vulnerability::EW.swap(false), Vulnerability::EW));
+    assert!(matches!(ALL.rotate(false), ALL));
+    assert!(matches!(NONE.rotate(false), NONE));
+    assert!(matches!(Vulnerability::NS.rotate(false), Vulnerability::NS));
+    assert!(matches!(Vulnerability::EW.rotate(false), Vulnerability::EW));
 };
 
 /// Par score and contracts
