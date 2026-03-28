f5a46ad1491a4ac6522c672e6ad54aca9a404e5d
Chen-Pang He <chen.pang.he@jdh8.org>
2026-03-28 15:15:39 +0800

    Just generate deals

---
+++ examples/generate-deals/README.md
@@ -0,0 +1,5 @@
+Just generate deals
+
+Usage: `cargo run --example generate-deals [n]`  
+where `n` is the number of deals to generate  
+(default: 100).
---
+++ examples/generate-deals/main.rs
@@ -0,0 +1,22 @@
+use dds_bridge::deal::{Deal, Seat};
+use std::process::ExitCode;
+
+fn generate_deals(n: usize) {
+    (0..n).for_each(|_| println!("{}", Deal::new(&mut rand::rng()).display(Seat::North)));
+}
+
+#[doc = include_str!("README.md")]
+fn main() -> ExitCode {
+    match std::env::args().nth(1) {
+        Some(string) => {
+            if let Ok(n) = string.parse::<usize>() {
+                generate_deals(n);
+            } else {
+                eprintln!("{}", include_str!("README.md"));
+                return ExitCode::FAILURE;
+            }
+        }
+        None => generate_deals(100),
+    }
+    ExitCode::SUCCESS
+}
