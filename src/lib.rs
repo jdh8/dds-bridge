b4efaad65677426d8909101855d242aa6e72ea63
Chen-Pang He <chen.pang.he@jdh8.org>
2026-04-13 17:10:26 +0800

    Remove rather internal Strain::SYS

--- src/lib.rs
+++ src/lib.rs
@@ -122,15 +122,6 @@ impl Strain {
         Self::Diamonds,
         Self::Clubs,
     ];
-
-    /// Strains in the order in [`dds_bridge_sys`]
-    pub const SYS: [Self; 5] = [
-        Self::Spades,
-        Self::Hearts,
-        Self::Diamonds,
-        Self::Clubs,
-        Self::Notrump,
-    ];
 }
 
 /// A suit of playing cards
